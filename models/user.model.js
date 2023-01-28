b49bc1fd963003d3e61a19bb6352f6324879008c
OmAxiani0 <aximaniom@gmail.com>
2023-01-28 23:44:11 +0530

    Added user model

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const { Schema } = mongoose;
+
+const UserSchema = Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  followers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: false,
+  },
+  following: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: false,
+  },
+});
+
+UserSchema.pre("save", {
+  async function(next) {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
+    next();
+  },
+});
+
+UserSchema.methods.validatePassword = async (pass) => {
+  return await bcrypt.compare(pass, this.password);
+};
