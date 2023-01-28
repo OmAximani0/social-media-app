0e2ede638f5dcba56e1dd6e95e26c2362fed4d45
OmAxiani0 <aximaniom@gmail.com>
2023-01-28 21:00:26 +0530

    Added logger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan")
 
 // Configure environment variable
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(morgan("dev"))
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Social Media App");
