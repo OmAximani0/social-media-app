fa9d987a954036cf2c0e799d58168fc31075e82b
OmAxiani0 <aximaniom@gmail.com>
2023-01-29 18:00:31 +0530

    Added middlewares to the route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,42 @@
 const userRoutes = require("express").Router();
+const passport = require("passport");
 const UserController = require("../controllers/user.controller");
+const { jwtExist } = require("../middlewares/jwt");
 
 userRoutes.post("/users", UserController.create);
-userRoutes.get("/users/:username", UserController.getUser);
-userRoutes.get("/users/:username/followers", UserController.getFollowers);
-userRoutes.get("/users/:username/following", UserController.getFollowing);
-userRoutes.post("/users/:username/follow", UserController.follow);
-userRoutes.delete("/users/:username/follow", UserController.unfollow);
+
+userRoutes.get(
+  "/users/:username",
+  jwtExist,
+  passport.authenticate("verify", { session: false }),
+  UserController.getUser
+);
+
+userRoutes
+  .route("/users/:username/follow")
+  .post(
+    jwtExist,
+    passport.authenticate("verify", { session: false }),
+    UserController.follow
+  )
+  .delete(
+    jwtExist,
+    passport.authenticate("verify", { session: false }),
+    UserController.unfollow
+  );
+
+userRoutes.get(
+  "/users/:username/followers",
+  jwtExist,
+  passport.authenticate("verify", { session: false }),
+  UserController.getFollowers
+);
+
+userRoutes.get(
+  "/users/:username/following",
+  jwtExist,
+  passport.authenticate("verify", { session: false }),
+  UserController.getFollowing
+);
 
 module.exports = userRoutes;
